39869e1623a193d9af1c492dd27ebcf5f12fc867
simofane <simofane4@gmail.com>
2021-11-16 15:02:52 +0100

    second

--- task_mg/home/views.py
+++ task_mg/home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def home(request):
+    return render(request,'index.html')
+
+def event(request):
+    return render(request,"event.html")
+
+def pers(request):
+    return render(request,'pers.html')
--- task_mg/task_mg/urls.py
+++ task_mg/task_mg/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import home ,event,pers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home,name="home"),
+    path('event',event,name="event"),
+    path('pers',pers , name='pers'),
 ]
